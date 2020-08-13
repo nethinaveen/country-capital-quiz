955c7c33a241e8cc405ddbfea315a7c4b00c3b11
nethinaveenkumar <nethinaveen.inc@gmail.com>
2020-08-13 00:00:03 -0400

    Added form validation

--- src/QuestionAndOptions.js
+++ src/QuestionAndOptions.js
@@ -8,15 +8,21 @@ const QuestionAndOptions = (props) => {
   const [option1, option2, option3, option4] = props.options;
   const [selectedAnswer, setSelectedAnswer] = useState("");
   const [correct, setCorrect] = useState(null);
+  const [err, setErr] = useState(false);
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(`handle submit ${e}, ${selectedAnswer}`);
-    if (capital === selectedAnswer) {
-      setCorrect("Yes");
+    console.log(`handle submit ${selectedAnswer}`);
+    setErr(false);
+    if (!selectedAnswer) {
+      setErr(true);
     } else {
-      setCorrect("No");
+      if (capital === selectedAnswer) {
+        setCorrect("Yes");
+      } else {
+        setCorrect("No");
+      }
     }
   };
 
@@ -36,6 +42,9 @@ const QuestionAndOptions = (props) => {
   } else if (correct && correct !== "" && correct === "No") {
     result = <h4 style={{ color: "#9a092c" }}>Wrong answer</h4>;
   }
+  if (err) {
+    result = <h4 style={{ color: "#9a092c" }}>Please select an answer</h4>;
+  }
 
   let submitButton,
     nextButton = "";
@@ -47,75 +56,84 @@ const QuestionAndOptions = (props) => {
 
   return (
     <div>
-      <h3>Question: What is the capital city of "{country}" ?</h3>
-      <table width="50%">
-        <tbody>
-          <tr>
-            <td>
-              <label>
-                A.{" "}
-                <input
-                  type="radio"
-                  value={option1}
-                  name="answer"
-                  checked={selectedAnswer === option1}
-                  onChange={(e) => setSelectedAnswer(e.target.value)}
-                />{" "}
-                {option1}
-              </label>
-            </td>
-            <td>
-              <label>
-                B.{" "}
-                <input
-                  type="radio"
-                  value={option2}
-                  name="answer"
-                  checked={selectedAnswer === option2}
-                  onChange={(e) => setSelectedAnswer(e.target.value)}
-                />{" "}
-                {option2}
-              </label>
-            </td>
-          </tr>
+      <form>
+        <h3>Question: What is the capital city of "{country}" ?</h3>
+        <table width="50%">
+          <tbody>
+            <tr>
+              <td>
+                <label>
+                  A.{" "}
+                  <input
+                    type="radio"
+                    value={option1}
+                    name="answer"
+                    checked={selectedAnswer === option1}
+                    onChange={(e) => setSelectedAnswer(e.target.value)}
+                  />{" "}
+                  {option1}
+                </label>
+              </td>
+              <td>
+                <label>
+                  B.{" "}
+                  <input
+                    type="radio"
+                    value={option2}
+                    name="answer"
+                    checked={selectedAnswer === option2}
+                    onChange={(e) => setSelectedAnswer(e.target.value)}
+                  />{" "}
+                  {option2}
+                </label>
+              </td>
+            </tr>
 
-          <tr>
-            <td>
-              <label>
-                C.{" "}
-                <input
-                  type="radio"
-                  value={option3}
-                  name="answer"
-                  checked={selectedAnswer === option3}
-                  onChange={(e) => setSelectedAnswer(e.target.value)}
-                />{" "}
-                {option3}
-              </label>
-            </td>
-            <td>
-              <label>
-                D.{" "}
-                <input
-                  type="radio"
-                  value={option4}
-                  name="answer"
-                  checked={selectedAnswer === option4}
-                  onChange={(e) => setSelectedAnswer(e.target.value)}
-                />{" "}
-                {option4}
-              </label>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <br />
-      {result}
+            <tr>
+              <td>
+                <label>
+                  C.{" "}
+                  <input
+                    type="radio"
+                    value={option3}
+                    name="answer"
+                    checked={selectedAnswer === option3}
+                    onChange={(e) => setSelectedAnswer(e.target.value)}
+                  />{" "}
+                  {option3}
+                </label>
+              </td>
+              <td>
+                <label>
+                  D.{" "}
+                  <input
+                    type="radio"
+                    value={option4}
+                    name="answer"
+                    checked={selectedAnswer === option4}
+                    onChange={(e) => setSelectedAnswer(e.target.value)}
+                  />{" "}
+                  {option4}
+                </label>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        {result}
+        <br />
+        <table width="50%">
+          <tbody>
+            <tr>
+              <td>{submitButton}</td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
       <br />
       <table width="50%">
         <tbody>
           <tr>
-            <td>{submitButton}</td>
             <td>{nextButton}</td>
           </tr>
         </tbody>
