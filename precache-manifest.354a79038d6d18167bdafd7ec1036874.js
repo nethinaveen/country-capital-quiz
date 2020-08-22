8812d684100e6f9d97f81ca3ac9ff9074fe3b9b2
nethinaveenkumar <nethinaveen.inc@gmail.com>
2020-08-22 18:57:20 -0400

    Updates

---
+++ precache-manifest.354a79038d6d18167bdafd7ec1036874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b65aea1eb707c35573962c32761b66",
+    "url": "/country-capital-quiz/index.html"
+  },
+  {
+    "revision": "5f3b7a64d5f7f72a59ce",
+    "url": "/country-capital-quiz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6898aeb291dadfb93fdd",
+    "url": "/country-capital-quiz/static/js/2.aad73681.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/country-capital-quiz/static/js/2.aad73681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3b7a64d5f7f72a59ce",
+    "url": "/country-capital-quiz/static/js/main.446892e1.chunk.js"
+  },
+  {
+    "revision": "ce2c783933ce8209b93e",
+    "url": "/country-capital-quiz/static/js/runtime-main.b562cfb9.js"
+  }
+]);
